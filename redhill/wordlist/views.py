f5fd93a2b76c6f89a5970e1ef0dba8c0958980b6
Tom Kobialka <tom@lambertlabs.com>
2019-04-29 17:17:42 +0100

    basic app v0.3

--- redhill/wordlist/views.py
+++ redhill/wordlist/views.py
@@ -1,10 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import WordForm
+from .models import Word
 
 
 def homepage(request):
     if request.method == 'POST':
         form = WordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path_info)
     form = WordForm()
-    return render(request, 'home.html', context={'form': form})
+    return render(request, 'home.html', context={'form': form, 'words': Word.objects.all()})
