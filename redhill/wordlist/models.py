224f84cf347abeb5dd5681d4afb5a9f3749e78b2
Tom Kobialka <tom@lambertlabs.com>
2019-04-29 17:52:26 +0100

    basic app v1

--- redhill/wordlist/models.py
+++ redhill/wordlist/models.py
@@ -4,6 +4,3 @@ from django.db import models
 class Word(models.Model):
     name = models.CharField(max_length=30, blank=False)
     definition = models.TextField(blank=False)
-
-    def get_items(self):
-            yield ((f.name, f.value_from_object(self)) for f in Word._meta.get_fields())
